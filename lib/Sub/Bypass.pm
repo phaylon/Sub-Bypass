742f6a5746681c655963dc5cf585f0aa0b17a37a
Robert 'phaylon' Sedlacek <rs@474.at>
2011-08-20 19:26:54 +0200

    lots of fixes and more tests

--- lib/Sub/Bypass.pm
+++ lib/Sub/Bypass.pm
@@ -6,9 +6,7 @@ package Sub::Bypass;
 
 use B;
 use B::Generate                         1.37;
-use B::Utils                            0.15;
 use B::Flags                            0.04;
-use B::Hooks::EndOfScope                0.09;
 use Check::UnitCheck                    0.13;
 use B::Hooks::OP::Check::EntersubForCV  0.06;
 use Sub::Install                        0.925   qw( install_sub );
@@ -22,7 +20,7 @@ use Sub::Exporter 0.982 -setup => {
 };
 
 sub install_bypassed_sub {
-    my ($package, $name) = @_;
+    my ($package, $name, %callback) = @_;
     install_sub {
         into => $package,
         as   => $name,
@@ -32,11 +30,13 @@ sub install_bypassed_sub {
     B::Hooks::OP::Check::EntersubForCV->import($package->can($name), sub {
         my ($code, $op) = @_;
         push @ops, $op;
+        $callback{on_parse}->($op)
+            if $callback{on_parse};
     });
     Check::UnitCheck::unitcheckify(sub {
         for my $op (@ops) {
-#            warn "OP FLAGS ", $op->flags, ' ', $op->flagspv;
-#            $op->dump;
+            _debug('*', transform => _id($op));
+            _dump(before => $op);
             my ($list) = $op->kids;
             my $next = $op->next;
             my $gv = _iterate($list->first, sub {
@@ -46,35 +46,72 @@ sub install_bypassed_sub {
                 }
                 return undef;
             });
-#            warn "RECURSING";
             _recurse($list->first, sub {
                 my $curr = shift;
+                my $prev = shift;
+                _debug(rec => _id($curr));
                 if (${$curr->next} == $$gv) {
-#                    warn "FLAGS ", $curr->flags, ' ', $curr->flagspv;
+                    _debug(target => _id($curr), before => _id($gv));
                     _transfer_context($op, $curr);
-#                    $curr->flags($curr->flags - 3)
-#                        if $op->flagspv =~ m/WANT_UNKNOWN/;
-#                    warn "BETWEEN ", $curr->flags, ' ', $curr->flagspv;
-#                    $curr->flags($gv->flags);
-#                    $curr->flags($curr->flags - 1)
-#                        if $op->flagspv =~ m/WANT_SCALAR/;
-#                    warn "AFTER ", $curr->flags, ' ', $curr->flagspv;
-                    $curr->next($next);
+                    if ($prev->name =~ m/^pad.v$/) {
+                        _debug(pad => _id($prev));
+                        _transfer_context($op, $prev);
+                    }
+                    _connect($curr, $next);
                 }
                 return undef;
             });
-            $gv->type(0);
-            $list->first->type(0);
-            $op->type(0);
-#            $op->dump;
+            _nullify($gv);
+            _debug(list_first => _id($list->first));
+            _nullify($list->first);
+            my $lfn = $list->first->next;
+            if ($lfn->name eq 'enter') {
+                _debug(list_first_next => _id($lfn));
+            }
+            _nullify($op);
+            _dump(after => $op);
             namespace::clean->clean_subroutines($package, $name);
         }
     });
 }
 
+sub _dump  {
+    return unless $ENV{SYNTAX_LET_DUMP};
+    print uc(shift), ' ';
+    (shift)->dump;
+}
+
+sub _debug {
+    return unless $ENV{SYNTAX_LET_DEBUG};
+    warn sprintf "%s\n", join ' ', @_;
+}
+
+sub _connect {
+    my ($op, $next) = @_;
+    _debug(connect => _id($op), '=>', _id($next));
+    $op->next($next);
+    return 1;
+}
+
+sub _id {
+    my ($op) = @_;
+    return sprintf 'gv[%s]', $op->gv->NAME
+        if $op->name eq 'gv';
+    return $op->name;
+}
+
+sub _nullify {
+    my ($op) = @_;
+    _debug(nulled => _id($op));
+    $op->type(0);
+    return 1;
+}
+
 sub _transfer_context {
     my ($from, $to) = @_;
+    _debug(flags_before => _id($to), $to->flagspv);
     $to->flags(($to->flags - _get_context($to)) + _get_context($from));
+    _debug(flags_after => _id($to), $to->flagspv);
     return 1;
 }
 
@@ -100,19 +137,15 @@ sub _iterate {
 
 sub _recurse {
     my ($search, $run, $seen) = @_;
-#    warn "REC";
     $seen ||= {};
     while (my $current = $search->next) {
-#        warn "LOOP " . $$current;
         return unless $current->can('name');
         return if $$current == $$search;
         unless ($seen->{ $$current }++) {
-#            warn "RUN ON " . $current->name;
-            $current->$run and return 1;
+            $current->$run($search) and return 1;
             if ($current->can('other') and $current->other) {
                 unless ($seen->{ ${$current->other} }++) {
-#                    warn "RUN ON " . $current->other->name;
-                    $current->other->$run and return 1;
+                    $current->other->$run($current) and return 1;
                     _recurse($current->other, $run, $seen) and return 1;
                 }
             }
--- t/basic.t
+++ t/basic.t
@@ -24,6 +24,23 @@ sub ctx { wantarray ? 'list' : 'scalar' }
 is let(23), 23, 'simple scalar';
 is +(my $s = let(@z)), 7, 'direct scalar context from assignment';
 
+do {
+    my $x = let(do { 23 });
+    is $x, 23, 'simple scalar';
+    my @y = qw( a b c d );
+    my $y = let(do { (my @f = qw( k l m n )); @f });
+    is $y, 4, 'simple scalar with array';
+    my $z = let(do { (my $x = 3); (my $y = 7); $x .. $y });
+    is $z, 7, 'range in scalar context';
+    my $h = let(do { my %h = (foo => 23); %h });
+    isnt $h, 23, 'hash in scalar context';
+    my $r = let(do { my $x = 23; $x ? (3 .. 6) : (2 .. 4) });
+    is $r, 6, 'dynamic range in scalar context';
+    # TODO causes read-only failure
+    #my $r2 = let(do { 3 .. 6 });
+    #is $r2, 6, 'static range in scalar context';
+};
+
 is scalar(let(do { ctx })), 'scalar', 'scalar context';
 is [let(do { ctx })]->[0], 'list', 'list context';
 
@@ -48,5 +65,36 @@ sub getreturnval {
 is getreturnval, 93, 'explicit return';
 
 ok not(__PACKAGE__->can('let')), 'not available as function';
+
+is let(let(23) + let(17)), 40, 'nested';
+is_deeply [let(let(23), let(24))], [23, 24], 'nested list';
+
+do {
+    my $x = 17;
+    my $y = 23;
+    is_deeply [let(do {
+            my ($n, $m) = ($x, $y);
+            let(do {my $j = $n; $j }),
+            let(do {my $k = $m; $k });
+        })],
+        [17, 23],
+        'nested list with scopes and vars';
+};
+
+do {
+    my $add = sub {
+        let(do {
+            (my $x = shift);
+            (my $y = shift);
+            return $x + $y unless shift;
+            'inner-fallback';
+        });
+        'outer-return';
+    };
+    my $val = $add->(11, 12);
+    is $val, 23, 'return from outer scope';
+    my $no_ret = $add->(11, 12, 1);
+    is $no_ret, 'outer-return', 'normal return';
+};
 
 done_testing;
